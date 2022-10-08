ef63e8b9916f6475766ccb40c7f3137e0afd3a2f
Ben Tucker <bwtucker4@gmail.com>
2022-10-08 13:36:41 -0700

    Sass stopped compiling again

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -5,6 +5,11 @@ const Title = () => {
     <div className='title-div'>
         <div className='title-text'>
             <div className='text-name'>Ben<br></br>Tucker</div>
+            <div className='blocks'>
+                <div className='block left-block'></div>
+                <div className='block right-block'></div>
+            </div>
+            <div className='bio'>frontend developer / chemist</div>
         </div>
     </div>
   )
