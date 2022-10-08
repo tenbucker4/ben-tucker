579ad5bb4181f68d78c04225e5ee284f362e1a3f
Ben Tucker <bwtucker4@gmail.com>
2022-10-08 14:49:20 -0700

    Added hamburger animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Icon from '@mdi/react'
 import { mdiGithub, mdiLink } from '@mdi/js';
 import { mdiLinkedin } from '@mdi/js';
@@ -6,26 +6,28 @@ import { mdiFileAccount } from '@mdi/js';
 import "../styles/header.css"
 
 const Header = () => {
-  return (
-    <header>
-        <div className='me'>Ben Tucker</div>
-        <div className='header-nav'>
-            <Icon path={mdiGithub}
-            size={1.5}
-            color="white"/>
-            <Icon path={mdiLinkedin}
-            size={1.5}
-            color="white"/>
-            <Icon path={mdiFileAccount}
-            size={1.5}
-            color="white"/>
-            <div className='hamburger'>
-                <div className='hamburger-bun top-bun'></div>
-                <div className='hamburger-bun bottom-bun'></div>
-            </div>
-        </div>
-    </header>
-  )
+	const [menuOpen, setMenuOpen] = useState(false)
+
+	return (
+		<header>
+			<div className='me'>Ben Tucker</div>
+			<div className='header-nav'>
+				<Icon path={mdiGithub}
+				size={1.5}
+				color="white"/>
+				<Icon path={mdiLinkedin}
+				size={1.5}
+				color="white"/>
+				<Icon path={mdiFileAccount}
+				size={1.5}
+				color="white"/>
+				<div className='hamburger' onClick={() => setMenuOpen(prev => !prev)}>
+					<span className={menuOpen ? "hamburger-bun top-bun active" : "hamburger-bun top-bun"}></span>
+					<span className={menuOpen ? "hamburger-bun bottom-bun active" : "hamburger-bun bottom-bun"}></span>
+				</div>
+			</div>
+		</header>
+	)
 }
 
 export default Header
