ad5ab73e9bb7c6a8d2e12720919d45f4ac871404
Ben Tucker <bwtucker4@gmail.com>
2022-10-09 09:36:50 -0700

    Finish benzene

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -12,7 +12,7 @@ const Title = () => {
             <div className='bio'>frontend developer / chemist</div>
         </div>
 		<div className='molecule-container'>
-			{/* <h1>PORTFOLIO</h1> */}
+			<h1>PORTFOLIO</h1>
 			<div className="molecule">
 				<div className='benzene-outer'>
           <div className='benzene-bond top-bond'></div>
