263c8f86e9e9327dcb15d3d0a712080b4f25d0db
Ben Tucker <bwtucker4@gmail.com>
2022-10-09 19:42:33 -0700

    Changed spacing in mobile view

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -6,12 +6,15 @@ const ProgressBar = ({ titleSection, projectSection, aboutSection, contactSectio
 	}
 
 	return (
-		<div className='progress-bar-container'>
-			<div className={titleInView? 'progress-bar bar-1 active' : 'progress-bar bar-1'} onClick={() => scroll(titleSection)}></div>
-			<div className={projectInView? 'progress-bar bar-2 active' : 'progress-bar bar-2'} onClick={() => scroll(projectSection)}></div>
-			<div className={aboutInView? 'progress-bar bar-3 active' : 'progress-bar bar-3'} onClick={() => scroll(aboutSection)}></div>
-			<div className={contactInView? 'progress-bar bar-4 active' : 'progress-bar bar-4'} onClick={() => scroll(contactSection)}></div>
+		<div className="progress">
+			<div className='progress-bar-container'>
+				<div className={titleInView? 'progress-bar bar-1 active' : 'progress-bar bar-1'} onClick={() => scroll(titleSection)}></div>
+				<div className={projectInView? 'progress-bar bar-2 active' : 'progress-bar bar-2'} onClick={() => scroll(projectSection)}></div>
+				<div className={aboutInView? 'progress-bar bar-3 active' : 'progress-bar bar-3'} onClick={() => scroll(aboutSection)}></div>
+				<div className={contactInView? 'progress-bar bar-4 active' : 'progress-bar bar-4'} onClick={() => scroll(contactSection)}></div>
+			</div>
 		</div>
+		
 	)
 }
 
