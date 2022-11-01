6c5347bedafa91feb2e59b611574bebff11972b1
Ben Tucker <bwtucker4@gmail.com>
2022-11-01 10:53:36 -0700

    Updatead project descriptions

--- src/projects/allProjects.js
+++ src/projects/allProjects.js
@@ -9,7 +9,7 @@ const allProjects = [
         title: "Click",
         type: "Instant Messaging Application",
         description:
-            "An instant messaging application featuring user authentication, text and image messaging, online and offline contact status, and profile customization.",
+            "An instant messaging application where users can send text and picture messages, see real-time online and offline contact status, and customize their profile.",
         techStack: "HTML, CSS, JavaScript, React, Firebase",
         codeLink: "https://github.com/tenbucker4/click",
         liveLink: "https://tenbucker4.github.io/click/",
@@ -19,7 +19,7 @@ const allProjects = [
         title: "YouClone",
         type: "YouTube Replica",
         description:
-            "A YouTube replica featuring category and manual search functionality for videos and channels and real-time subscriber and view count.",
+            "A YouTube replica where users can browse and view their favourite videos and channels, see real-time subscriber and like counts, and watch content on any device.",
         techStack: "HTML, CSS, JavaScript, React, MaterialUI, RapidAPI",
         codeLink: "https://github.com/tenbucker4/youclone",
         liveLink: "https://tenbucker4.github.io/youclone/",
@@ -29,7 +29,7 @@ const allProjects = [
         title: "The Range",
         type: "Shopping Cart Application",
         description:
-            "A mock e-commerce application for golfing equipment. Features include manual and category product search and add/edit/delete cart functionality.",
+            "A mock e-commerce application where users can add golfing equipment including drivers, irons, balls and gloves to their shopping cart.",
         techStack: "HTML, CSS, JavaScript, React",
         codeLink: "https://github.com/tenbucker4/the-range",
         liveLink: "https://tenbucker4.github.io/the-range/",
@@ -39,7 +39,7 @@ const allProjects = [
         title: "Ether",
         type: "Educational Space App",
         description:
-            "An informative app where users can learn more about out solar system. Real-time data such as number of known moons and averages temperatures are displayed using The Solar System OpenData public API.",
+            "An educational app where users can learn more about out solar system. Real-time data such as average temperature and number of discovered moons are displayed for each planet in the solar system.",
         techStack: "HTML, CSS, JavaScript, React, SASS",
         codeLink: "https://github.com/tenbucker4/ether",
         liveLink: "https://tenbucker4.github.io/ether/",
